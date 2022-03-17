d1966c817a63428804284a1fa0851a3ec477be30
armand <armand.salle@gmail.com>
2022-03-17 10:43:08 +0100

    Fix scrolling body

--- src/components/mobileMenu.ts
+++ src/components/mobileMenu.ts
@@ -1,5 +1,5 @@
 export default function initMobileMenuComponent() {
-  const { body } = document
+  // const pageWrapper = document.querySelector('.page-wrapper') as HTMLDivElement
 
   const menu = document.querySelector('.mobile-nav') as HTMLDivElement
   const openButton = document.querySelector('.mobile-menu-button-icon button.mobile-menu-button') as HTMLButtonElement
@@ -7,18 +7,41 @@ export default function initMobileMenuComponent() {
 
   const links = [...menu.querySelectorAll<HTMLAnchorElement>('.mobile-nav_middle a')]
 
-  const openAction = () => {
-    menu.style.display = 'flex'
-    body.style.overflowY = 'hidden'
-  }
-  const closeAction = () => {
+  // const openAction = () => {
+  //   menu.style.display = 'flex'
+  //   pageWrapper.style.overflow = 'hidden'
+  //   pageWrapper.style.height = '100vh'
+  // }
+  // const closeAction = () => {
+  //   menu.style.display = 'none'
+  //   pageWrapper.style.overflow = 'unset'
+  //   pageWrapper.style.height = 'auto'
+  // }
+
+  const { body } = document
+
+  const closeDialog = () => {
+    const scrollY = body.style.top
+    body.style.position = ''
+    body.style.top = ''
+    window.scrollTo(0, parseInt(scrollY || '0') * -1)
     menu.style.display = 'none'
-    body.style.overflowY = 'unset'
   }
 
-  openButton.addEventListener('click', openAction)
-  closeButton.addEventListener('click', closeAction)
+  const showDialog = () => {
+    menu.style.display = 'flex'
+    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y')
+    body.style.position = 'fixed'
+    body.style.top = `-${scrollY}`
+  }
+
+  openButton.addEventListener('click', showDialog)
+  closeButton.addEventListener('click', closeDialog)
   links.forEach((link) => {
-    link.addEventListener('click', closeAction)
+    link.addEventListener('click', closeDialog)
+  })
+
+  window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`)
   })
 }
