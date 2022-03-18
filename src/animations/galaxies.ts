90d940190ffddbd8eaa14096fcede59fef2d49b1
armand <armand.salle@gmail.com>
2022-03-18 16:06:04 +0100

    Add markers for debugiing

--- src/animations/galaxies.ts
+++ src/animations/galaxies.ts
@@ -42,7 +42,15 @@ export default function initGalaxiesAnimation() {
 
   items.forEach((item) => {
     const circle = item.querySelector('.galaxies-component_circle') as HTMLDivElement
-
+    // const path = item.querySelector('.galaxies-component_path path') as SVGPathElement
+    // gsap.set(circle, {
+    //   motionPath: {
+    //     path: path,
+    //     align: path,
+    //     alignOrigin: [0.5, 0.5],
+    //   },
+    //   immediateRender: true,
+    // })
     ScrollTrigger.create({
       trigger: item,
       animation: animation(item, circle),
@@ -51,6 +59,7 @@ export default function initGalaxiesAnimation() {
       end: '0% 50%',
       scrub: 0.2,
       immediateRender: true,
+      markers: true,
     })
   })
 }
