0612aa7134cf4ccfafdc27830b9837b3e9fa80d8
armand <armand.salle@gmail.com>
2022-03-18 14:30:16 +0100

    Try to fix safari reset scroll

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -1,9 +1,13 @@
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { BodyScrollOptions, disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+
+const options: BodyScrollOptions = {
+  reserveScrollBarGap: true,
+}
 
 export function stopScrolling() {
   const modal = document.querySelector<HTMLDivElement>('.js-modal')
   if (!modal) return
-  disableBodyScroll(modal)
+  disableBodyScroll(modal, options)
 }
 
 export function allowScrolling() {
